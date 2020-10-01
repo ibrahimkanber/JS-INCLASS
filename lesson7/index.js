db48043d2b5912cd6127aa823e11e3d25b5f7ef9
ibrahimKanber <ikanber45@gmail.com>
2020-10-01 21:17:13 +0200

    completed version1

--- lesson7/index.js
+++ lesson7/index.js
@@ -1,43 +1,41 @@
-const todoList=[];
-const todoListEl=document.querySelector("#myUL");
+const todoList = [];
+const todoListEl = document.querySelector("#myUL");
 
-document.querySelector("#todo_button").addEventListener("click",addTodo);
+document.querySelector("#todo_button").addEventListener("click", addTodo);
 
-function addTodo(){
-    const todoText=document.querySelector("#myInput").value;
-    console.log(todoText);
+function addTodo() {
+  const todoText = document.querySelector("#myInput").value;
+  console.log(todoText);
 
-    const todoObject={
-        id:todoList.length*2,
-        todoText:todoText,
-        isDone:false
-    };
+  const todoObject = {
+    id: todoList.length * 2,
+    todoText: todoText,
+    isDone: false,
+  };
 
-    todoList.push(todoObject);
-    displayTodos()
-
+  todoList.push(todoObject);
+  displayTodos();
 }
 
-function doneTode(todoId){
-    const selectedTodoId=todoList.findIndex(todo=>todo.id==todoId);
+function doneTode(todoId) {
+  const selectedTodoId = todoList.findIndex((todo) => todo.id == todoId);
 
-    todoList[selectedTodoId].isDone=true;
-
-
+  todoList[selectedTodoId].isDone = true;
 }
 
-function displayTodos(){
-    todoList.forEach(todo=>{
-        const todoElement=document.createElement("li");
-        todoElement.dataset.id="deneme";
-        if(item.isDone){
-            todoElement.classList.add("checked")
-        }
-        todoElement.addEventListener()
-        todoListEl.appendChild(todoElement)
-    })
+function displayTodos() {
+  todoList.forEach((todo) => {
+    const todoElement = document.createElement("li");
+    todoElement.dataset.id = "deneme";
+    if (item.isDone) {
+      listElement.classList.add("checked");
+    }
+
+    listElement.addEventListener("click", function (e) {
+      const selectedId = e.target.getAttribute("data-id");
+      doneTodo(selectedId);
+    });
+
+    todoListElement.appendChild(listElement);
+  });
 }
-
-
-
-
