b5692c760758dfdfa4cf44903b203d22bd248eb0
ibrahimKanber <ikanber45@gmail.com>
2020-10-02 21:38:45 +0200

    lesson2

---
+++ lesson2/script.js
@@ -0,0 +1,42 @@
+const myVar_1 = 6;
+const myVar_2 = 1000000;
+const myVar_3 = 3.5;
+const myVar_4 = 0;
+const myVar_5 = "6";
+const myVar_6 = "Merhaba !";
+const myVar_7 = "Selamlar! " + 105;
+const myVar_8 = 5 > 2;
+const myVar_9 = Boolean(undefined);
+const myVar_10 = myVar_1 == myVar_5
+const myVar_11 = myVar_1 === myVar_5
+console.log(`myVar_1: ${myVar_1} typeof: ${typeof myVar_1}`)
+console.log(`myVar_2: ${myVar_2} typeof: ${typeof myVar_2}`)
+console.log(`myVar_3: ${myVar_3} typeof: ${typeof myVar_3}`)
+console.log(`myVar_4: ${myVar_4} typeof: ${typeof myVar_4}`)
+console.log(`================================`)
+console.log(`myVar_5: ${myVar_5} typeof: ${typeof myVar_5}`)
+console.log(`myVar_6: ${myVar_6} typeof: ${typeof myVar_6}`)
+console.log(`myVar_7: ${myVar_7} typeof: ${typeof myVar_7}`)
+console.log(`================================`)
+console.log(`myVar_8: ${myVar_8} typeof: ${typeof myVar_8}`)
+console.log(`myVar_9: ${myVar_9} typeof: ${typeof myVar_9}`)
+console.log(`myVar_10: ${myVar_10} typeof: ${typeof myVar_10}`)
+console.log(`myVar_11: ${myVar_11} typeof: ${typeof myVar_11}`)
+const myValue_1 = 10;
+const myValue_2 = "7";
+const result_1 = myValue_1 + myValue_2;
+const result_2 = myValue_1 - myValue_2;
+
+console.log(`RESULT: ${result_1} typeof: ${typeof result_1}`);
+console.log(`RESULT: ${result_2} typeof: ${typeof result_2}`);
+console.log("hello.".search("."))
+console.log("hello.".indexOf("."))
+
+console.log(Boolean(""))
+
+
+
+
+
+
+
